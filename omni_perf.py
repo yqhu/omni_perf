98c766b75a28aca8ff615f8481a92fb9ec3f6931
Patrcik Hu <patrickhu@fb.com>
2021-11-18 03:56:04 +0000

    Add multi GPU support

--- omni_perf.py
+++ omni_perf.py
@@ -16,36 +16,40 @@ import drawSvg as draw
 import svg_stack as ss
 
 
-def create_svg(svg_name, info, label1, label2, WIDTH=1200, HEIGHT=200):
+def create_svg(svg_name, info, label1, label2, WIDTH=1200, HEIGHT=200, cpu_max=None, mem_max=None):
     d_cpu = draw.Drawing(WIDTH, HEIGHT, displayInline=False)
 
     x_offset = WIDTH * 0.009
     x_scale = (WIDTH - 2 * x_offset) / info[-1][0]
     y_scale = HEIGHT / 200
-    cpu_max = max([item[1] for item in info])
-    mem_max = max([item[2] for item in info])
+    c_max = max([item[1] for item in info])
+    if not cpu_max: cpu_max = c_max
+    m_max = max([item[2] for item in info])
+    if not mem_max: mem_max = m_max
     for i in range(1, len(info)):
         prev = info[i - 1]
         line = info[i]
         x0 = x_scale * prev[0] + x_offset
         x1 = x_scale * line[0] + x_offset
         # CPU
-        y = 0.475 * HEIGHT / cpu_max * prev[1]
-        y_offset = HEIGHT / 2
-        r = draw.Rectangle(x0, 0 + y_offset, x1 - x0, y, fill='#83e4eb')
-        r.appendTitle(f'{label1}: {prev[1]:.1f}')
-        d_cpu.append(r)
+        if cpu_max > 0:
+            y = 0.475 * HEIGHT / cpu_max * prev[1]
+            y_offset = HEIGHT / 2
+            r = draw.Rectangle(x0, 0 + y_offset, x1 - x0, y, fill='#83e4eb')
+            r.appendTitle(f'{label1}: {prev[1]:.1f}')
+            d_cpu.append(r)
         # MEM
         y_offset = HEIGHT
-        y = 0.475 * HEIGHT / mem_max * prev[2]
-        r = draw.Rectangle(x0, 0, x1 - x0, y, fill='#33ffc4')
-        r.appendTitle(f'{label2}: {prev[2]:.1f}') 
-        d_cpu.append(r)
+        if mem_max > 0:
+            y = 0.475 * HEIGHT / mem_max * prev[2]
+            r = draw.Rectangle(x0, 0, x1 - x0, y, fill='#33ffc4')
+            r.appendTitle(f'{label2}: {prev[2]:.1f}') 
+            d_cpu.append(r)
         
     d_cpu.append(draw.Rectangle(x_scale * info[0][0] + x_offset, 0 + HEIGHT/2, WIDTH - 2*x_offset, 1, fill='#83e4eb'))
-    d_cpu.append(draw.Text(label1 + f' (Max = {cpu_max:.1f}%)', 32, x_offset, HEIGHT/2 + 30, fill='white'))
+    d_cpu.append(draw.Text(label1 + f' (Max = {c_max:.1f}%)', 32, x_offset, HEIGHT/2 + 30, fill='white'))
     d_cpu.append(draw.Rectangle(x_scale * info[0][0] + x_offset, 0, WIDTH - 2*x_offset, 1, fill='#33ffc4'))
-    d_cpu.append(draw.Text(label2 + f' (Max = {mem_max:.1f}%)', 32, x_offset, 0 + 30, fill='white'))
+    d_cpu.append(draw.Text(label2 + f' (Max = {m_max:.1f}%)', 32, x_offset, 0 + 30, fill='white'))
 
     d_cpu.saveSvg(svg_name)
 
@@ -56,32 +60,42 @@ class SysInfo:
         self.flag = flag
         self.interval = interval
         self.info = []
-        self.info_gpu = []
+        self.info_gpu = {}
     
     def start(self):
         try:
             pynvml.nvmlInit()
             gpu_handle =  pynvml.nvmlDeviceGetHandleByIndex(0)
-            has_gpu = pynvml.nvmlDeviceGetCount() > 0
+            num_gpus = pynvml.nvmlDeviceGetCount()
         except:
-            has_gpu = False
+            num_gpus = 0
+
+        for i in range(num_gpus):
+            self.info_gpu[i] = []
 
         start = time.time()
         while self.flag.value:
             now = time.time() - start
             self.info.append((now, psutil.cpu_percent(), psutil.virtual_memory().percent))
-            if has_gpu:
-                gpu_handle =  pynvml.nvmlDeviceGetHandleByIndex(0)
+            for i in range(num_gpus):
+                gpu_handle =  pynvml.nvmlDeviceGetHandleByIndex(i)
                 gpu = pynvml.nvmlDeviceGetUtilizationRates(gpu_handle)
                 mem = pynvml.nvmlDeviceGetMemoryInfo(gpu_handle)
                 mem = 100 * mem.used / mem.total
-                self.info_gpu.append((now, gpu.gpu, mem))
+                self.info_gpu[i].append((now, gpu.gpu, mem))
             # TODO: adjust sleep duration based on actual timing
             time.sleep(self.interval)
 
-        create_svg(self.output + '_cpu.svg', self.info, '% CPU', '% RAM')
-        if has_gpu:
-            create_svg(self.output + '_gpu.svg', self.info_gpu, '% GPU', '% G-RAM')
+        create_svg(self.output + '_cpu.svg', self.info, '% CPU', '% MEM')
+
+        c_max = 0
+        m_max = 0
+        for i in range(num_gpus):
+            c_max = max(c_max, max([item[1] for item in self.info_gpu[i]]))
+            m_max = max(m_max, max([item[2] for item in self.info_gpu[i]]))
+        for i in range(num_gpus):
+            create_svg(self.output + f'_gpu_{i}.svg', self.info_gpu[i], f'% GPU {i}', f'% GMEM {i}', 
+            cpu_max=c_max, mem_max=m_max)
 
 
 def main():
@@ -131,7 +145,8 @@ def main():
     print('System activity sampling frequency:', int(1/options.sys_interval), 'Hz')
     try:
         pynvml.nvmlInit()
-        print('Number of GPUs:', pynvml.nvmlDeviceGetCount())
+        num_gpus = pynvml.nvmlDeviceGetCount()
+        print('Number of GPUs:', num_gpus)
     except:
         print('No GPU found')
     print('=' * 40)
@@ -179,11 +194,18 @@ def main():
 
     layout.addSVG(outname + '_cs.svg')
     layout.addSVG(outname + '_cpu.svg')
-    if os.path.exists(outname + '_gpu.svg'):
-        layout.addSVG(outname + '_gpu.svg')
+    for i in range(num_gpus):
+        if os.path.exists(outname + f'_gpu_{i}.svg'):
+            layout.addSVG(outname + f'_gpu_{i}.svg')
 
     doc.setLayout(layout)
     doc.save(outname + '.svg')
+
+    os.remove(outname + '_cs.svg')
+    os.remove(outname + '_cpu.svg')
+    for i in range(num_gpus):
+        if os.path.exists(outname + f'_gpu_{i}.svg'):
+            os.remove(outname + f'_gpu_{i}.svg')
 
 
 if __name__ == '__main__':
